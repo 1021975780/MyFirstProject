dcc6d4cc075e0ccc8d5164fa3b82887de2526a4b
王群 <1021975780@qq.com>
2019-09-28 14:15:31 +0800

    6

--- src/main/java/hub/Apple.java
+++ src/main/java/hub/Apple.java
@@ -4,11 +4,8 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println(2);
 		System.out.println(1);
-<<<<<<< HEAD
 		System.out.println(3);
 		System.out.println("»»¸ö·ÖÖ§");
-=======
 		System.out.println(6);
->>>>>>> refs/remotes/origin/master
 	}
 }
